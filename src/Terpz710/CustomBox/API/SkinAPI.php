c41b502055665ce0dc7c06774a152c05e2a5c98e
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-09-13 00:48:22 -0700

    Update SkinAPI.php

--- src/Terpz710/CustomBox/API/SkinAPI.php
+++ src/Terpz710/CustomBox/API/SkinAPI.php
@@ -9,13 +9,25 @@ class SkinAPI
     public static function pngToBytes(string $path): string
     {
         $img = @imagecreatefrompng($path);
+        if (!$img) {
+            throw new \RuntimeException("Failed to create image from PNG file at path: " . $path);
+        }
+        
         $bytes = "";
-        for ($y = 0; $y < (int)@getimagesize($path)[1]; $y++) {
-            for ($x = 0; $x < (int)@getimagesize($path)[0]; $x++) {
+        $imageSize = @getimagesize($path);
+        if (!$imageSize) {
+            throw new \RuntimeException("Failed to get image size for file at path: " . $path);
+        }
+
+        [$width, $height] = $imageSize;
+        
+        for ($y = 0; $y < $height; $y++) {
+            for ($x = 0; $x < $width; $x++) {
                 $rgba = @imagecolorat($img, $x, $y);
                 $bytes .= chr(($rgba >> 16) & 0xff) . chr(($rgba >> 8) & 0xff) . chr($rgba & 0xff) . chr(((~((int)($rgba >> 24))) << 1) & 0xff);
             }
         }
+
         @imagedestroy($img);
         return $bytes;
     }
